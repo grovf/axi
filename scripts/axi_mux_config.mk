fb2851d35037b1ef326c51fd90b51056892dceb3
Anton S <anton.s@grovf.com>
2023-10-04 12:56:39 +0400

    added assembly scrupts for mux and demux

---
+++ scripts/axi_mux_config.mk
@@ -0,0 +1,33 @@
+DESIGN_CONFIG_PATH ?= .
+RTL_SV_SRC ?=
+RTL_SV_INCDIRS ?=
+
+RTL_ROOT := $(PULP_COMMON_PATH)/src
+
+RTL_SV_SRC += $(RTL_ROOT)/cf_math_pkg.sv
+RTL_SV_SRC += $(RTL_ROOT)/spill_register.sv
+RTL_SV_SRC += $(RTL_ROOT)/spill_register_flushable.sv
+RTL_SV_SRC += $(RTL_ROOT)/fifo_v3.sv
+RTL_SV_SRC += $(RTL_ROOT)/counter.sv
+RTL_SV_SRC += $(RTL_ROOT)/delta_counter.sv
+RTL_SV_SRC += $(RTL_ROOT)/rr_arb_tree.sv
+RTL_SV_SRC += $(RTL_ROOT)/addr_decode_dync.sv
+RTL_SV_SRC += $(RTL_ROOT)/addr_decode.sv
+RTL_SV_SRC += $(RTL_ROOT)/lzc.sv
+
+RTL_ROOT := $(DESIGN_CONFIG_PATH)/../src
+
+RTL_SV_SRC += $(RTL_ROOT)/axi_pkg.sv
+RTL_SV_SRC += $(RTL_ROOT)/axi_cut.sv
+RTL_SV_SRC += $(RTL_ROOT)/axi_err_slv.sv
+RTL_SV_SRC += $(RTL_ROOT)/axi_multicut.sv
+RTL_SV_SRC += $(RTL_ROOT)/shift_reg.sv
+RTL_SV_SRC += $(RTL_ROOT)/axi_id_prepend.sv
+RTL_SV_SRC += $(RTL_ROOT)/axi_mux.sv
+
+RTL_SV_DEFINES +=
+
+RTL_SV_INCDIRS += $(DESIGN_CONFIG_PATH)/../include
+RTL_SV_INCDIRS += $(PULP_COMMON_PATH)/include
+
+RTL_TOP := axi_mux
